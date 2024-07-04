b943cad04f87732bcf84ab53f5cad0ba0bd6bebd
Lani <ooluwalani@gmail.com>
2024-07-04 04:58:06 -0400

    Refactoring - fetch games with react query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import genres from '../data/genres';
 import { useQuery } from '@tanstack/react-query';
-import { FetchResponse } from './useData';
+import { FetchResponse } from "../services/api-client";
 import apiClient from '../services/api-client';
 
 export interface Genre {
@@ -9,12 +9,6 @@ export interface Genre {
   image_background: string;
 }
 
-// const useGenres = () => useData<Genre>('/genres')
-
-// here we switch from calling the endpoint to fetch genres and 
-// instead use the static data saved in the data folder
-
-// const useGenres = () => ({ data: genres, isLoading: false, error: null })
 const useGenres = () => useQuery({
   queryKey: ['genres'],
   queryFn: () => apiClient
@@ -23,13 +17,5 @@ const useGenres = () => useQuery({
   staleTime: 24 * 60 * 60 * 1000, // 24h,
   initialData: { count: genres.length, results: genres }
 })
-
-// const fetchGenres = () => {
-//   return useQuery<Genre[], Error>({
-//     queryKey: ['genres'],
-//     queryFn: () => useData<Genre>('/genres')
-//     // staleTime: 10 * 1000,
-//   })
-// }
 
 export default useGenres;
